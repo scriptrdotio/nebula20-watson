8ac9802c6ae68854b99c930ad0401f3d34b115c3
scriptrdotio <10049864+scriptrdotio@users.noreply.github.com>
2018-12-13 19:22:21 +0200

    Update and rename apps/nebula/bluemix/bluemix.h to apps/nebula/watson/watson.h

--- apps/nebula/watson/watson.h
+++ apps/nebula/watson/watson.h
@@ -2,6 +2,6 @@
  * config file for connectivity with scriptr.io
  ******************************************************/
 #define MQTT_BROKER_ADDRESS                 "quickstart.messaging.internetofthings.ibmcloud.com"
-#define PUB_TOPIC                           "iot-2/evt/scriptr-R85F8F8C8E/fmt/json"
-#define CLIENT_ID                           "d:quickstart:nebula20:nebula20R85F8F8C8E"
+#define PUB_TOPIC                           "iot-2/evt/scriptr-<CLIENT_ID>/fmt/json"
+#define CLIENT_ID                           "d:quickstart:nebula20:nebula20<CLIENT_ID>"
 #define DEVICE_ID                           "nebula20" //default, replace if you are connecting a second device
