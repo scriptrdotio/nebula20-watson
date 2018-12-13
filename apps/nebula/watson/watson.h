30163f2b4b43139fc055194875c8934e7bcbb5d7
scriptrdotio <10049864+scriptrdotio@users.noreply.github.com>
2018-12-13 19:34:10 +0200

    Update watson.h

--- apps/nebula/watson/watson.h
+++ apps/nebula/watson/watson.h
@@ -1,7 +1,7 @@
 /******************************************************
- * config file for connectivity with scriptr.io
+ * config file for connectivity with Watson IoT
  ******************************************************/
 #define MQTT_BROKER_ADDRESS                 "quickstart.messaging.internetofthings.ibmcloud.com"
-#define PUB_TOPIC                           "iot-2/evt/scriptr-<CLIENT_ID>/fmt/json"
-#define CLIENT_ID                           "d:quickstart:nebula20:nebula20<CLIENT_ID>"
+#define PUB_TOPIC                           "iot-2/evt/scriptr-<TOKEN>/fmt/json"
+#define CLIENT_ID                           "d:quickstart:nebula20:nebula20<TOKEN>"
 #define DEVICE_ID                           "nebula20" //default, replace if you are connecting a second device
