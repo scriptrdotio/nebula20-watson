16ce75bd43453038bc8bee7a08816df63c67a418
julienmradn <julien.mrad@elementn.com>
2019-03-01 17:47:16 +0200

    Update watson.h

--- apps/nebula/watson/watson.h
+++ apps/nebula/watson/watson.h
@@ -4,4 +4,4 @@
 #define MQTT_BROKER_ADDRESS                 "quickstart.messaging.internetofthings.ibmcloud.com"
 #define PUB_TOPIC                           "iot-2/evt/scriptr-<TOKEN>/fmt/json"
 #define CLIENT_ID                           "d:quickstart:nebula20:nebula20<TOKEN>"
-#define DEVICE_ID                           "nebula20" //default, replace if you are connecting a second device
+#define DEVICE_ID                           "myDevice" //default, replace if you are connecting a second device
