d7f36f51908068b8d3999b982b4b62603c76ab65
touta <marvette.feghali@elementn.com>
2019-03-19 15:36:04 +0200

    Update watson.h

--- apps/nebula/watson/watson.h
+++ apps/nebula/watson/watson.h
@@ -3,5 +3,5 @@
  ******************************************************/
 #define MQTT_BROKER_ADDRESS                 "quickstart.messaging.internetofthings.ibmcloud.com"
 #define PUB_TOPIC                           "iot-2/evt/scriptr-<TOKEN>/fmt/json"
-#define CLIENT_ID                           "d:quickstart:sensors:deviceDigest<TOKEN>"
+#define CLIENT_ID                           "d:quickstart:sensors:device<TOKEN>"
 #define DEVICE_ID                           "myDevice" //default, replace if you are connecting a second device
